ca3c62968614774c69020f7d2af603cd8d304685
Cagla Caglayan <cagla.caglayan@kordsa.com>
2022-10-24 15:29:31 +0300

    correction 2

---
+++ MVC_proje/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_proje.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetCategoryList()
+        {
+            List<Category> categoryValues = cm.GetAllBL();
+            return View(categoryValues);
+        }
+    }
+}
