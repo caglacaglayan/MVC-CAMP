9189a201eaeafe4b8885fc62fd5456f2c27c94f3
Cagla Caglayan <cagla.caglayan@kordsa.com>
2022-10-24 15:08:20 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
